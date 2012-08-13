668c412fbb9468f6dcf52dfaf1a35133d173015b
Ivan Willig <ivan@msl.home>
2012-08-13 09:13:39 -0400

    Changed the main uploader page based on feed back from Jeff.

--- geonode/static/script/layer_upload.js
+++ geonode/static/script/layer_upload.js
@@ -1,47 +1,13 @@
+var file_list = $('#file-list');
 
-var build_input = function(options) {
-    return $('<input />', {
-        type: 'file',
-        name: options.name,
-    })
-};
+var load_file_selections = function(data) {    
+    // whats the difference between original files and files.
+    var files = data.files;
 
-var dbf_file = build_input({
-     name: 'dbf_file'
-});
-
-var prj_file = build_input({
-    name: 'prj_file'
-});
-
-var shx_file = build_input({
-    name: 'shx_file'
-});
-
-
-var load_shapefile_inputs = function() {
-    var main_form = $('#upload_form');
-    dbf_file.appendTo(main_form);
-    prj_file.appendTo(main_form);
-    shx_file.appendTo(main_form);
-};
-
-var disable_shapefile_inputs = function() {
-    dbf_file.remove();
-    prj_file.remove();
-    shx_file.remove();
-};
-
-var do_main_action = function(event, data) {
-    // get the first and only file in the form at this point
-    var main_file = data.files[0],
-        main_name = main_file.name.toLowerCase();
-    
-    if ((/\.shp$/i).test(main_name)) {
-        load_shapefile_inputs();
-    } else {
-        disable_shapefile_inputs();
-    }; 
+    for (var i = 0; i< files.length; i++) {
+        var file = files[i];
+        $('<p/>', {text: file.name}).appendTo(file_list);
+    };
 
 };
 
@@ -55,8 +21,13 @@ var setup = function(options) {
 
     form.fileupload({
         dataType: 'json',
-        done: function(e, data) {
-            do_main_action(e, data);
+        dropZone: $('#dropzone'),
+        drop: function(e, data) {
+            load_file_selections(data);
+
+        },
+        add: function(e, data) {
+            load_file_selections(data);
 
         }
         
