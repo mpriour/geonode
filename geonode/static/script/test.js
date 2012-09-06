4f030f95667043eff726ef8b6a3e4e11e0c3d18e
Ivan Willig <iwillig@gmail.com>
2012-09-06 11:40:23 -0400

    Make the test module play nicely with jslint.

--- geonode/static/script/test.js
+++ geonode/static/script/test.js
@@ -1,5 +1,6 @@
+/*global setup:true, types: true, test: true, ok:true, equal:true, get_ext: true, get_name: true, FileType: true, find_file_type: true*/
 
-
+'use strict';
 
 module('main tests', {
     setup: function () {
@@ -8,20 +9,22 @@ module('main tests', {
 });
 
 test('test split the file name', function () {
-    equal(get_ext({name: 'hello.shp'}), 'shp');
-    equal(get_name({name: 'hello.shp'}), 'hello');
+
+    equal(get_ext({name: 'hello.shp'}), 'shp', 'Make sure that we get the correct extension');
+    equal(get_name({name: 'hello.shp'}), 'hello', 'Make sure we get the correct file name');
 });
 
-test('test creating a filetype object', function() {
-    var blah = new FileType('Blah type', 'blah', ['blah', 'more-blah']);
+test('test creating a filetype object', function () {
+    var blah = new FileType('Blah type', 'blah', ['blah', 'more-blah']),
+        esri = find_file_type({'name': 'hello.shp'}),
+        blah_type = find_file_type({'name': 'hello.blah'});
+
     equal(blah.name, 'Blah type');
-    ok(blah.is_type({name:'file.blah'}));
+    ok(blah.is_type({name: 'file.blah'}), 'Double check that we can correctly identify a file type');
 
     // locate a shapefile
-    var esri = find_file_type({'name': 'hello.shp'});
     equal(typeof esri, "object");
-    
-    var blah_type = find_file_type({'name': 'hello.blah'});
+
     equal(typeof blah_type, 'undefined');
 
     // make sure we have enough types
@@ -29,5 +32,5 @@ test('test creating a filetype object', function() {
     // add our blah type
     types.push(blah);
     equal(types.length, 4);
-    
+
 });
