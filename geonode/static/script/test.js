ff631d113503436d4ec34cbcceaca9bad8d39e53
Ivan Willig <iwillig@gmail.com>
2012-09-12 15:33:11 -0400

    Added checks to the prepare_form_data method to make sure that it is populated correctly.

--- geonode/static/script/test.js
+++ geonode/static/script/test.js
@@ -10,29 +10,35 @@ test('Test the FileType object', function () {
     ok(type instanceof up.FileType, 'Does new return the correct object');
     strictEqual(type.main, 'test', 'Is the main type correct');
     strictEqual(type.name, 'Test Type', 'Is the name correct');
-    ok(type.is_type('file.test'), 'Can the type identify its own type');
+    ok(type.is_type({name: 'file.test'}), 'Can the type identify its own type');
 
     deepEqual(type.find_type_errors(['req']), [], 'Can the type check errors correctly');
 
 });
 
 module('Test LayerInfo on Shapefile');
-test('Test LayerInfo object on a error free Shapefile', function () {
+test('Test LayerInfo object on a valid free Shapefile', function () {
     var shape_info = new up.LayerInfo(
         'nybb',
-        ['nybb.shp', 'nybb.dbf', 'nybb.prj', 'nybb.shx']
+        [{name: 'nybb.shp'}, {name: 'nybb.dbf'}, {name: 'nybb.prj'}, {name: 'nybb.shx'}]
     ),
-        form_data = shape_info.prepare_form_data();
+        res = {},
+        mock_form_data = {append: function (key, value) { res[key] = value; }};
+
+    shape_info.prepare_form_data(mock_form_data);
+    ok(res.hasOwnProperty('base_file'));
+    ok(res.hasOwnProperty('permissions'));
+    ok(res.hasOwnProperty('prj_file'));
+    ok(res.hasOwnProperty('dbf_file'));
+    ok(res.hasOwnProperty('shx_file'));
 
     ok(shape_info instanceof up.LayerInfo, 'Make sure the constructor returns the correct object');
     // our test file is an shapefile
     strictEqual(shape_info.type, up.shp, 'Make sure we identify the correct type');
 
     deepEqual(shape_info.get_extensions(), ['shp', 'dbf', 'prj', 'shx'], 'Find the correct extensions');
-    strictEqual(shape_info.main, 'nybb.shp');
 
-    ok(form_data instanceof FormData, 'The prepare_form_data method needs return a FormData');
-
+    strictEqual(shape_info.main.name, 'nybb.shp');
     // check the errors array
     strictEqual(shape_info.errors.length, 0);
     // make sure that all of the associated files are correct
@@ -41,7 +47,7 @@ test('Test LayerInfo object on a error free Shapefile', function () {
 });
 
 test('Test LayerInfo on an invalid shapefile', function () {
-    var bad_info = new up.LayerInfo('nybb', ['nybb.shp']);
+    var bad_info = new up.LayerInfo('nybb', [{name: 'nybb.shp'}]);
     strictEqual(bad_info.type, up.shp);
     strictEqual(bad_info.errors.length, 3);
 
@@ -50,19 +56,32 @@ test('Test LayerInfo on an invalid shapefile', function () {
 
 module('Test LayerInfo on a CSV file');
 test('Test LayerInfo object on an valid CSV file', function () {
-    var csv_info = new up.LayerInfo('test-csv', ['test.csv']);
+    var csv_info = new up.LayerInfo('test-csv', [{name: 'test.csv'}]),
+        res = {},
+        mock_form_data = {append: function (key, value) { res[key] = value; }};
+
+    csv_info.prepare_form_data(mock_form_data);
 
     ok(csv_info instanceof up.LayerInfo, 'The constructor should return the correct type');
     strictEqual(csv_info.type, up.csv, 'Make sure that a csv is correctly identified');
     deepEqual(csv_info.errors.length, 0, 'There should be no errors');
-    strictEqual(csv_info.main, 'test.csv', 'This should be the main file');
-    ok(csv_info.prepare_form_data(), 'Make that we can generate a form data object');
+    strictEqual(csv_info.main.name, 'test.csv', 'This should be the main file');
+    ok(csv_info.prepare_form_data(mock_form_data), 'Make that we can generate a form data object');
 
+    ok(res.hasOwnProperty('base_file'));
+    ok(res.hasOwnProperty('permissions'));
 });
 
 module('Test the LayerInfo object on a tiff file');
 test('Test the LayerInfo object on a valid tiff file', function () {
-    var tif_info = new up.LayerInfo('test-tif', ['test.tif']);
+    var tif_info = new up.LayerInfo('test-tif', [{name: 'test.tif'}]),
+        res = {},
+        mock_form_data = {append: function (key, value) { res[key] = value; }};
+
+    tif_info.prepare_form_data(mock_form_data);
+    ok(res.hasOwnProperty('base_file'));
+    ok(res.hasOwnProperty('permissions'));
+
     ok(tif_info instanceof up.LayerInfo, 'Make sure the constructor returns the correct type');
     strictEqual(tif_info.type, up.tif, 'Make sure the type returned is correct');
     deepEqual(tif_info.errors.length, 0);
@@ -72,12 +91,17 @@ test('Test the LayerInfo object on a valid tiff file', function () {
 
 module('Test the LayerInfo object on a zip file');
 test('Test the LayerInfo object on a valid zip file', function () {
-    var zip_info = new up.LayerInfo('test-zip', ['test.zip']);
+    var zip_info = new up.LayerInfo('test-zip', [{name: 'test.zip'}]),
+        res = {},
+        mock_form_data = {append: function (key, value) { res[key] = value; }};
+
+    zip_info.prepare_form_data(mock_form_data);
+
+    ok(res.hasOwnProperty('base_file'));
+    ok(res.hasOwnProperty('permissions'));
 
     ok(zip_info instanceof up.LayerInfo, 'Make sure the constructor returns the correct type');
     strictEqual(zip_info.type, up.zip);
     deepEqual(zip_info.errors.length, 0);
-
-    ok(zip_info.prepare_form_data() instanceof FormData);
 
 });
