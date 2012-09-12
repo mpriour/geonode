f3828ec3cb6788c2566908231215fbbce5449a2e
Ivan Willig <iwillig@gmail.com>
2012-09-12 10:16:10 -0400

    Updated the tests, a LayerInfo object requires at least one other file besides the main. This is a bug and needs to be fixed.

--- geonode/static/script/test.js
+++ geonode/static/script/test.js
@@ -2,26 +2,27 @@
 'use strict';
 var up = UPLOAD;
 
-test('FileType', function () {
+test('Test the FileType object', function () {
     var type = new up.FileType('Test Type', 'test', ['req']);
 
     ok(type instanceof up.FileType, 'Does new return the correct object');
     equal(type.main, 'test', 'Is the main type correct');
-    equal(type.name, 'Test Type');
-    ok(type.is_type({name: 'file.test'}), 'Can the type identify its own type');
-    // equal(type.find_type_errors(['req']), []);
+    equal(type.name, 'Test Type', 'Is the name correct');
+    ok(type.is_type('file.test'), 'Can the type identify its own type');
+
+    deepEqual(type.find_type_errors(['req']), [], 'Can the type check errors correctly');
 });
 
 
 test('Test LayerInfo object', function () {
-    var info = new up.LayerInfo('nybb', [{name: 'nybb.shp'}]),
+    var info = new up.LayerInfo('nybb', ['nybb.shp', 'nybb.dbf']),
         form_data = info.prepare_form_data();
 
     ok(info instanceof up.LayerInfo);
     // our test file is an shapefile
     equal(info.type, up.types[0]);
 
-    deepEqual(info.get_extensions(), ["shp"]);
+    deepEqual(info.get_extensions(), ['shp', 'dbf']);
 
     ok(form_data instanceof FormData);
 
