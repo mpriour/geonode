1738a40877221f1194a83ac1088b495ab1be695f
Matt Priour <mpriour@opengeo.org>
2012-10-10 16:59:48 -0500

    Refactor printing urls and views to produce a preview image and reduce possible url collision

--- geonode/printing/views.py
+++ geonode/printing/views.py
@@ -1,42 +1,96 @@
 import urllib2
 from django.conf import settings
 from django.http import HttpResponse
-from django.utils import simplejson as json
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_http_methods
-from django.template import Context, RequestContext, loader, Template
+from django.template import RequestContext, loader, Template
 from django.core import serializers
 from geonode.maps.models import Map
 from geonode.layers.models import Layer
 from geonode.printing.models import PrintTemplate
 
 
-def printing_print(request, templateid, mapid=None, layerid=None):
+def printing_print_map(request, templateid, mapid=None):
+    resource_context = get_resource_context(mapid=mapid)
+    return printing_print(request, templateid, resource_context)
+
+
+def printing_print_layer(request, templateid, layerid=None):
+    resource_context = get_resource_context(layerid=layerid)
+    return printing_print(request, templateid, resource_context)
+
+
+def printing_preview_map(request, templateid, mapid=None):
+    resource_context = get_resource_context(mapid=mapid)
+    return printing_preview(request, templateid, resource_context)
+
+
+def printing_preview_layer(request, templateid, layerid=None):
+    resource_context = get_resource_context(layerid=layerid)
+    return printing_preview(request, templateid, resource_context)
+
+
+def printing_print(request, templateid, resource_context):
     """interpolate the template with the given id"""
-    template_obj = get_object_or_404(PrintTemplate, pk=templateid)
-    resource_obj = None
+    template = get_template(templateid)
+    rendered = render_template(request, template, resource_context)
+    # try:
+    printed = urllib2.urlopen(settings.GEOSERVER_PRINT_URL + "pdf", rendered)
+    # except Exception, e:
+    #     return HttpResponse(
+    #         e.message,
+    #         mimetype="text/plain",
+    #         status=e.code or 500
+    #     )
+    return HttpResponse(printed, content_type="application/pdf")
+
+
+def printing_preview(request, templateid, resource_context):
+    """interpolate the template with the given id"""
+    template = get_template(templateid)
+    rendered = render_template(request, template, resource_context)
+    # try:
+    printed = urllib2.urlopen(settings.GEOSERVER_PRINT_URL + "png", rendered)
+    # except Exception, e:
+    #     return HttpResponse(
+    #         e.message,
+    #         mimetype="text/plain",
+    #         status=e.code or 500
+    #     )
+    return HttpResponse(printed, content_type="image/png")
+
+
+def get_resource_context(mapid=None, layerid=None):
     if mapid is not None:
         resource_obj = get_object_or_404(Map, pk=mapid)
     else:
         resource_obj = get_object_or_404(Layer, typename=layerid)
+    resource_map = (mapid is not None) and {"map": resource_obj} or {"layer": resource_obj}
+    return resource_map
+
+
+def get_template(templateid):
+    template_obj = get_object_or_404(PrintTemplate, pk=templateid)
 
     if template_obj.contents:
         template = Template(template_obj.contents)
     else:
-        try:
-            if not template_obj.url.find("http", 0, 4) > -1:
-                template = loader.get_template(template_obj.url)
-            else:
-                remote_template = urllib2.urlopen(template_obj.url)
-                template = Template(remote_template.readlines())
-        except Exception, e:
-            return HttpResponse(
-                "Error reading or parsing remote template at %s" % template_obj.url,
-                mimetype="text/plain",
-                status=500
-            )
-    resource_map = (mapid is not None) and {"map": resource_obj} or {"layer": resource_obj}
-    context = RequestContext(request, resource_map)
+        # try:
+        if not template_obj.url.find("http", 0, 4) > -1:
+            template = loader.get_template(template_obj.url)
+        else:
+            remote_template = urllib2.urlopen(template_obj.url)
+            template = Template(remote_template.readlines())
+        # except Exception, e:
+        #     return HttpResponse(
+        #         "Error reading or parsing remote template at %s" % template_obj.url,
+        #         mimetype="text/plain",
+        #         status=500
+        #     )
+    return template
+
+
+def render_template(request, template, resource_context):
+    context = RequestContext(request, resource_context)
     try:
         rendered = template.render(context)
     except Exception, e:
@@ -45,16 +99,8 @@ def printing_print(request, templateid, mapid=None, layerid=None):
             mimetype="text/plain",
             status=500
         )
+    return rendered
 
-    try:
-        printed = urllib2.urlopen(settings.GEOSERVER_PRINT_URL, rendered)
-    except Exception, e:
-        return HttpResponse(
-            e.message,
-            mimetype="text/plain",
-            status=e.code or 500
-        )
-    return HttpResponse(printed, content_type="application/pdf")
 
 #require_GET()
 def printing_template_list(request):
@@ -63,4 +109,4 @@ def printing_template_list(request):
     return HttpResponse(
         serializers.serialize("json", templates),
         mimetype="application/json"
-    );
+    )
